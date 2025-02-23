9b68b559f07b560f5e7e74810abe11ce6c40378f
Mylari KR <kmylari@gmail.com>
2025-02-23 13:13:44 +0000

    Working solution

--- minimum_delete_valid_paranthesis.py
+++ minimum_delete_valid_paranthesis.py
@@ -1,24 +1,23 @@
-class Solution:
-    def minRemoveToMakeValid(self, s: str) -> str:
-        stack = []
-        res = []
-        for i in range(len(s)):
-            if s[i] == "(":
-                stack.append(i)
-                res.append(s[i])
-            elif s[i] == ")":
-                if stack:
-                    stack.pop()
-                    res.append(s[i])
-            else:
-                res.append(s[i])
-        while stack:
-            x = stack.pop()
-            del res[x]
-        return "".join(res)
+def minRemoveToMakeValid(s: str) -> str:
+    stack = []
+    res = []
+    for c in s:
+        if c == "(":
+            res.append(c)
+            stack.append(len(res)-1)
+        elif c == ")":
+            if stack:
+                stack.pop()
+                res.append(c)
+        else:
+            res.append(c)
+    while stack:
+        x = stack.pop()
+        del res[x]
+    return "".join(res)
 
-if __name__ == "main":
+if __name__ == '__main__':
   test_strings = ["lee(t(c)o)de)", "a)b(c)d", "))(("]
   for s in test_strings:
-    res = Solution().minRemoveToMakeValid(s)
+    res = minRemoveToMakeValid(s)
     print(res)
